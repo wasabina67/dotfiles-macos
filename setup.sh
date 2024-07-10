4c43ebfe513881ee623dd53c482a174c7ac0f623
wasabina67 <wasabina67@gmail.com>
2024-07-10 18:47:41 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
+#!/bin/zsh
 
+FILES=(
+    ".gitconfig"
+    ".zprofile"
+    ".zsh_aliases"
+    ".zshrc"
+)
+DOTFILES_DIR=""
+
+for f in "${FILES[@]}"; do
+    if [ ! -e "${HOME}/${f}" ]; then
+        ln -s "${DOTFILES_DIR}/${f}" "${HOME}/${f}"
+    fi
+done
